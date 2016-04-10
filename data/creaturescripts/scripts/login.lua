7ac7f13bf11bac505bf08af478b4dbadd20ea438
Non Sequitur <cezarguimaraes2810@gmail.com>
2016-04-10 01:11:09 -0300

    bonus exp stages

--- data/creaturescripts/scripts/login.lua
+++ data/creaturescripts/scripts/login.lua
@@ -1,4 +1,42 @@
+currentExpBonus = 1
+local bonusExpStages = {
+	{1, 1.04}, -- 4% bonus
+	{2, 1.05},
+	{4, 1.07},
+}
+
+local bonusDays = {
+	[1] = true, -- Domingo
+	[7] = true, -- Sábado
+}
+
 function onLogin(player)
+	local dt = os.date('*t')
+	if bonusDays[dt.wday] then 
+		local nplayers = #Game.getPlayers()
+		local changed = false
+		for k, stage in ipairs(bonusExpStages) do
+			if nplayers >= stage[1] then
+				changed = true
+				if currentExpBonus < stage[2] then
+					Game.broadcastMessage("The server has reached " .. stage[1] .. " players online! The bonus of exp is now " .. ((stage[2] - 1) * 100) .. "%!")
+				elseif currentExpBonus > stage[2] then
+					Game.broadcastMessage("The exp bonus has been reduced because there aren't enough players online. Current bonus is " .. ((stage[2] - 1) * 100) .. "%!")
+				end
+				currentExpBonus = stage[2]
+				break
+			end
+		end
+
+		if not changed and currentExpBonus > 1 then
+			print 'deact'
+			currentExpBonus = 1
+			Game.broadcastMessage("The exp bonus has been deactivated because there aren't enough players online.")
+		end
+	else
+		currentExpBonus = 1
+	end
+
 	local loginStr = "Welcome to " .. configManager.getString(configKeys.SERVER_NAME) .. "!"
 	if player:getLastLoginSaved() <= 0 then
 		loginStr = loginStr .. " Please choose your outfit."
